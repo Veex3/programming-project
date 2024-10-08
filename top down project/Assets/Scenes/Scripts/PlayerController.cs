23e382559ef0a0d336765ca51ac48a02ed957d7d
Veex3 <idalis.soto26@gmail.com>
2024-10-08 11:21:44 -0400

    new code

--- top down project/Assets/Scenes/Scripts/PlayerController.cs
+++ top down project/Assets/Scenes/Scripts/PlayerController.cs
@@ -1,17 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    //public rigidbody2d rb;
+    private SpriteRenderer sr;
+    public bool hasKey = false;
 
+
+    //sprite variables
+    public Sprite upSprite;
+    public Sprite leftSprite;
+    public Sprite rightSprite;
+    public Sprite frontSprite;
+
+    //public Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -19,27 +24,50 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 newPosition = transform.position;
 
-        if (Input.GetKey("d"))
-        {
-            newPosition.x += speed;
-        }
-
-        if (Input.GetKey("a"))
-        {
-            newPosition.x -= speed;
-        }
-
+        //go up
         if (Input.GetKey("w"))
         {
             newPosition.y += speed;
+            //change sprite to up sprite
+            sr.sprite = upSprite;
         }
 
+        //go left
+        if (Input.GetKey("a"))
+        {
+            newPosition.x -= speed;
+            sr.sprite = leftSprite;
+        }
+
+        //go down
         if (Input.GetKey("s"))
         {
             newPosition.y -= speed;
+            sr.sprite = frontSprite;
+        }
+
+        //go right
+        if (Input.GetKey("d"))
+        {
+            newPosition.x += speed;
+            sr.sprite = rightSprite;
         }
 
         transform.position = newPosition;
     }
 
-}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("key"))
+        {
+            hasKey = true; //key get
+            Debug.Log("gotcha!!");
+        }
+
+        //check if colliding with a game object with specific tag
+        if (collision.gameObject.tag.Equals("door1"))
+        {
+            Debug.Log("change scene");
+            SceneManager.LoadScene(1);
+        }
+    }
